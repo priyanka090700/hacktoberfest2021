6e69732345b7e3946b851a189a3c250bbad261d5
Vibhanshu Garg <64217477+vibhanshu2001@users.noreply.github.com>
2021-10-16 10:01:05 +0530

    Add files via upload

---
+++ CPP/radix_sort.cpp
@@ -0,0 +1,55 @@
+//Radix Sort
+/*Enter the number of elements: 10
+Enter the maximum digit of elements: 3
+Enter elements:
+802 630 20 745 52 300 612 932 78 187
+Data before Sorting: 802 630 20 745 52 300 612 932 78 187
+Data after Sorting: 20 52 78 187 300 612 630 745 802 932*/
+#include<iostream>
+#include<list>
+#include<cmath>
+using namespace std;
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
+}
+void radixSort(int *arr, int n, int max) {
+   int i, j, m, p = 1, index, temp, count = 0;
+   list<int> pocket[10];      //radix of decimal number is 10
+   for(i = 0; i< max; i++) {
+      m = pow(10, i+1);
+      p = pow(10, i);
+      for(j = 0; j<n; j++) {
+         temp = arr[j]%m;
+         index = temp/p;      //find index for pocket array
+         pocket[index].push_back(arr[j]);
+      }
+      count = 0;
+      for(j = 0; j<10; j++) {
+         //delete from linked lists and store to array
+         while(!pocket[j].empty()) {
+            arr[count] = *(pocket[j].begin());
+            pocket[j].erase(pocket[j].begin());
+            count++;
+         }
+      }
+   }
+}
+int main() {
+   int n, max;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   cout << "Enter the maximum digit of elements: ";
+   cin >> max;
+   int arr[n]; //create an array with given number of elements
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i<n; i++) {
+      cin >> arr[i];
+   }
+   cout << "Data before Sorting: ";
+   display(arr, n);
+   radixSort(arr, n, max);
+   cout << "Data after Sorting: ";
+   display(arr, n);
+}
